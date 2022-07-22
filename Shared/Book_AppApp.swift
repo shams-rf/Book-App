4fe0c98f379d612d5915b9cfafe5166063121072
Shamsuddin Refaei <shamss.aldin@gmail.com>
2022-07-22 17:14:45 +0100

    Initial Commit

---
+++ Shared/Book_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Book_AppApp.swift
+//  Shared
+//
+//  Created by Shamsuddin Refaei on 22/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct Book_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
